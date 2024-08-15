02810961ca8c90a495bece01d9f1d388656d3568
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-08-15 21:48:22 +0600

    json data find mongodb

--- index.js
+++ index.js
@@ -28,13 +28,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+
+
+
+
+    const ProductCollections = client.db('Ph-job-task').collection('product');
+
+
+    // product related api 
+    app.get('/products', async(req, res)=>{
+        const result = await ProductCollections.find().toArray();
+        res.send(result);
+    })
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
